e527728a5b3180fbec32c98023b1757c4c52571d
Flavio Pinto <flaviopnt@hotmail.it>
2020-03-31 16:07:09 +0200

    Files di base

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/**
+ * Chiedi all’utente il cognome
+ * inseriscilo in un array con altri cognomi
+ * stampa la lista ordinata alfabeticamente
+ * Scrivi anche la posizione della lista in cui il nuovo utente si trova
+ */
+
